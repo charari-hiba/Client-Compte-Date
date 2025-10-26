9680b199721e93d0af72aebf78042e433ae4a01c
charari-hiba <hiba5charari@gmail.com>
2025-10-26 12:05:56 +0100

    Update Compte.h
    Updated Compte class to include date of creation and changed account number type.

--- Compte.h
+++ Compte.h
@@ -1,17 +1,24 @@
 #pragma once
 #include<iostream>
 using namespace std;
+#include"Date.h"
+namespace bank{
 
 class Compte
 {
 	private:
-	string numeroCompte;
+    static int count;
+	int numeroCompte;
 	double solde;
 	string devise;
+    Date* datecreation;
 public:
 	
-	Compte(string, double, string);
+	Compte(string, double, Date*);
 	void afficherCompte() const;
+    void debiter(double montant);
+    void crediter(double montant);
 	~Compte();
 };
 
+}
