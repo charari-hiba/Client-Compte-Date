a63b1e8b52ea9f03678ba914738f081238864786
charari-hiba <hiba5charari@gmail.com>
2025-10-26 12:30:39 +0100

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -1,31 +1,55 @@
 #include "Client.h"
+using namespace std;
+using namespace bank;
+
+int Client::ninstance = 0;
+int Client::idcount = 0;
 
 
 
-Client::Client(string nom, string prenom, string cin, Compte* c, Date d)
+Client::Client(string nom, string prenom, string cin, Compte* c, Date *d)
 	: nom(nom), prenom(prenom), CIN(cin), compte(c), dateNaissance(d)
 {
+	ninstance++;
+    idcount++;
+    numclient = idcount;
 	cout << "costructeur client" << endl;
 }
 
-/*Client::Client(string nom, string prenom, string cin, Compte& c, Date d)
-	: nom(nom), prenom(prenom), CIN(cin), compte(c), dateNaissance(d)
+
+Client Client::creator(string n, string p, string cin, Compte* c, Date* d)
 {
-}*/
+	if (ninstance < 3) { return Client(p, n, cin, c, d); }
+
+	else {
+		cout << "c'est la limite des clients " << endl;
+		exit;
+	}
+	
+}
 
 void Client::afficherClient() const
 {
+	cout << "numClient: " << numclient << endl;
 	cout << "Nom: " << nom << endl;
 	cout << "Prenom: " << prenom << endl;
 	cout << "CIN: " << CIN << endl;
+	
+	if (this->compte != nullptr) {
 	cout << "Compte : " << endl;
 	compte->afficherCompte();
-	//compte.afficherCompte();
+	}else 
+		cout << "erreur affichage compte" << endl;
+	
+	if(this->dateNaissance != nullptr) {
 	cout << "Date de Naissance: ";
-	dateNaissance.afficherDate();
+	dateNaissance->afficherDate();
+	}else
+		cout << "erreur affichage date" << endl;
 }
 
 Client::~Client()
 {
+	ninstance--;
 	cout << "destructeur client" << endl;
 }
